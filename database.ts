e4c09f7262db56d09f5f4bf30c6fddf1349ab675
E-M-B-E-R <138512336+E-M-B-E-R@users.noreply.github.com>
2024-10-25 15:16:35 -0400

    Same refactor work as previous commit but with other tables. This is not very DRY at all.

--- database.ts
+++ database.ts
@@ -52,21 +52,90 @@ const createTables = (db: DB, queries : Array<string>) => {
     });
 };
 
-const seedMonsters = (db: DB) => {
-    return db.query(`
-        INSERT INTO monsters (monster_id, name, alive_time_in_minutes, stage)
-        VALUES 
-            (NULL, 'Baby Gabo', 3, 1),
-            (NULL, 'Gabo', 10, 2),
-            (NULL, 'Super Gabo', 25, 3);
-    `);
+const monstersData = [
+    {
+        monster_id: 1,
+        name: 'Baby Gabo',
+        alive_time_in_minutes: 3,
+        stage: 1,
+    },
+    {
+        monster_id: 2,
+        name: 'Gabo',
+        alive_time_in_minutes: 10,
+        stage: 2,    
+    },
+    {
+        monster_id: 3,
+        name: 'Super Gabo',
+        alive_time_in_minutes: 25,
+        stage: 3,
+    }
+];
+
+const monsterQueryString = `
+  INSERT INTO monsters (
+    monster_id, 
+    name, 
+    alive_time_in_minutes, 
+    stage
+    ) VALUES (
+      :monster_id,
+      :name,
+      :alive_time_in_minutes,
+      :stage
+    )
+`;
+
+const insertMonster = (db: DB, data: QueryParameterSet, query: string) => {
+    const insertMonsterQuery = db.prepareQuery<[], {
+        monster_id: number;
+        name: string;
+        alive_time_in_minutes: number;
+        stage: number;
+    }>(query);
+    insertMonsterQuery.execute(data);
+    insertMonsterQuery.finalize();
 };
 
-const seedUsers = (db: DB) => {
-    return db.query(`
-        INSERT INTO users (user_id, name, user_monster_id)
-        VALUES (NULL, 'E-M-B-E-R', NULL)
-    `);
+const seedMonsters = (db: DB, monsterList: Array<QueryParameterSet> ) => {
+    monsterList.forEach((monsterData) => {
+        insertMonster(db, monsterData, monsterQueryString);
+    });
+};
+
+const usersData = [{
+    user_id: 1,
+    name: "E-M-B-E-R", 
+    user_monster_id: 1
+}];
+
+const userQueryString = `
+  INSERT INTO users (
+    user_id, 
+    name,  
+    user_monster_id
+    ) VALUES (
+      :user_id,
+      :name,
+      :user_monster_id
+    )
+`;
+
+const insertUser = (db: DB, data: QueryParameterSet, query: string) => {
+    const insertUserQuery = db.prepareQuery<[], {
+        user_id: number;
+        name: string;
+        user_monster_id: number;
+    }>(query);
+    insertUserQuery.execute(data);
+    insertUserQuery.finalize();
+};
+
+const seedUsers = (db: DB, userList: Array<QueryParameterSet> ) => {
+    userList.forEach((userData) => {
+        insertUser(db, userData, userQueryString);
+    });
 };
 
 const evolutionRequirementsData = [{
@@ -82,14 +151,14 @@ const evolutionRequirementsData = [{
 
 const evolutionRequirementsQueryString = `
   INSERT INTO  evolution_requirements (
-            evolution_requirement_id, 
-            monster_id, 
-            evolution_id, 
-            percent_commits, 
-            percent_pull_requests_opened, 
-            percent_issues_opened, 
-            percent_pull_request_reviews, 
-            total_monster_age_in_minutes
+    evolution_requirement_id, 
+    monster_id, 
+    evolution_id, 
+    percent_commits, 
+    percent_pull_requests_opened, 
+    percent_issues_opened, 
+    percent_pull_request_reviews, 
+    total_monster_age_in_minutes
   ) VALUES (
     :evolution_requirement_id,
     :monster_id,  
@@ -122,11 +191,70 @@ const seedEvolutionRequirements = (db: DB, evolutionRequirementList: Array<Query
         insertEvolutionRequirement(db,evolutionRequirementData, evolutionRequirementsQueryString);
     });
 };
-        )
-    `);
+
+const usersMonstersData = [{
+    monster_id: 1,
+    nickname: "Hi", 
+    current_commits: 0,
+    current_pull_requests_opened:0,
+    current_opened_issues: 0,
+    current_pull_requests_reviews: 0,
+    is_frozen: 0,
+    monster_age_in_minutes: 1, 
+}]
+
+const userMonsterQueryString = `
+  INSERT INTO user_monsters (
+    user_monster_id, 
+    monster_id, 
+    nickname, 
+    current_commits, 
+    current_pull_requests_opened, 
+    current_opened_issues, 
+    current_pull_requests_reviews, 
+    is_frozen, 
+    monster_age_in_minutes 
+  ) VALUES (
+    :user_monster_id, 
+    :monster_id, 
+    :nickname, 
+    :current_commits, 
+    :current_pull_requests_opened, 
+    :current_opened_issues, 
+    :current_pull_requests_reviews, 
+    :is_frozen, 
+    :monster_age_in_minutes 
+   )
+`;
+
+const insertUserMonster = (db: DB, data: QueryParameterSet, query: string) => {
+    const insertUserMonsterQuery = db.prepareQuery<[], {
+        user_monster_id: number;
+        monster_id: number;
+        nickname: string;
+        current_commits: number;
+        current_pull_requests_opened: number; 
+        current_opened_issues: number;
+        current_pull_requests_reviews: number; 
+        is_frozen: number; 
+        monster_age_in_minutes: number;
+    }>(query);
+    insertUserMonsterQuery.execute(data);
+    insertUserMonsterQuery.finalize();
+};
+
+// These should be in a separate file from the seeding code. The seeding is really just for debugging, but inserting records in general is a feature.
+
+const seedUsersMonsters = (db: DB, userMonsterList: Array<QueryParameterSet> ) => {
+    userMonsterList.forEach((userMonsterData) => {
+        insertUserMonster(db, userMonsterData, userMonsterQueryString);
+    });
 };
 
 const seedTables = (db: DB) => {
+    seedMonsters(db, monstersData);
+    seedUsersMonsters(db, usersMonstersData);
+    seedUsers(db, usersData);
     seedEvolutionRequirements(db, evolutionRequirementsData);
 };
 
